1f2ac24b667f02ad823b25d5cb5ce44ccf5b747d
Akshat Singh Rajput <choturajput0@gmail.com>
2023-08-09 19:45:40 +0530

    Build Update and CI

--- server/app.js
+++ server/app.js
@@ -1,36 +1,44 @@
-const express = require('express');
-const verifyProduct = require('./routes/verifyProduct.routes.js');
-const registerProduct = require('./routes/product.routes.js');
-const registerCompany = require('./routes/company.routes.js');
-const registerRetailer = require('./routes/retailer.routes.js');
-const adminRoutes = require('./routes/admin.routes.js')
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const path = require("path");
+
 const app = express();
 
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config({
-        path: "config/config.env"
-    });
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config({
+    path: "config/config.env",
+  });
 }
 
 //using middlewares
 app.use(cors());
-app.use(express.json({
-    limit: '50mb'
-}));
-app.use(express.urlencoded({
-    limit: '50mb',
-    extended: true
-}));
+app.use(
+  express.json({
+    limit: "50mb",
+  })
+);
+app.use(
+  express.urlencoded({
+    limit: "50mb",
+    extended: true,
+  })
+);
 app.use(cookieParser());
-
+app.use(express.static(path.join(__dirname, "..", "./client", "build")));
 
 //using routes
-app.use('/api/v1', verifyProduct);
-app.use('/api/v1', registerProduct);
-app.use('/api/v1', registerCompany);
-app.use('/api/v1', registerRetailer);
-app.use('/api/v1/admin', adminRoutes)
+app.use("/api/v1", require("./routes/verifyProduct.routes.js"));
+app.use("/api/v1", require("./routes/product.routes.js"));
+app.use("/api/v1", require("./routes/company.routes.js"));
+app.use("/api/v1", require("./routes/retailer.routes.js"));
+app.use("/api/v1/admin", require("./routes/admin.routes.js"));
+
+// Serving the Build
+app.get("/*", (req, res) => {
+  return res.sendFile(
+    path.join(__dirname, "..", "./client", "build", "index.html")
+  );
+});
 
 module.exports = app;
